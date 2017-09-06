95bdaf5746c685cc7e0078623c0a844a6c49181d
James Thompson <jthompson@nav.com>
2017-09-06 16:49:04 -0600

    Completed second solution.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,49 +1,96 @@
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
-    else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
-    end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+require "delegate"
+
+module BuildItem
+  def for(item)
+    case item.name
+      when "Sulfuras, Hand of Ragnaros" then LegendaryItem.new(item)
+      when "Aged Brie" then AgedItem.new(item)
+      when "Backstage passes to a TAFKAL80ETC concert" then ConcertTickets.new(item)
+      when "Conjured Mana Cake" then ConjuredItem.new(item)
+      else NormalItem.new(item)
     end
   end
+  module_function :for
+end
+
+class BaseItem < SimpleDelegator
+  def update
+    update_quality
+    update_sell_in
+    update_quality if self.sell_in < 0
+  end
+
+  private
+
+  def update_quality
+    raise NotImplementedError
+  end
+
+  def update_sell_in
+    raise NotImplementedError
+  end
+
+  def noop; end
+
+  def decrement_sell_in
+    self.sell_in -= 1
+  end
+
+  def decrement_quality
+    self.quality -= 1 if self.quality > 0
+  end
+
+  def increment_quality
+    self.quality += 1 if self.quality < 50
+  end
+end
+
+class NormalItem < BaseItem
+  private
+
+  alias_method :update_quality, :decrement_quality
+  alias_method :update_sell_in, :decrement_sell_in
+end
+
+class LegendaryItem < NormalItem
+  private
+
+  alias_method :update_quality, :noop
+  alias_method :update_sell_in, :noop
+end
+
+class AgedItem < NormalItem
+  private
+
+  alias_method :update_quality, :increment_quality
+  alias_method :update_sell_in, :decrement_sell_in
+end
+
+class ConcertTickets < NormalItem
+  private
+
+  def update_quality
+    if self.sell_in < 0
+      self.quality = 0
+    else
+      increment_quality
+      increment_quality if self.sell_in < 11
+      increment_quality if self.sell_in < 6
+    end
+  end
+end
+
+class ConjuredItem < NormalItem
+  private
+
+  def update_quality
+    decrement_quality
+    decrement_quality
+  end
+end
+
+def update_quality(items)
+  items.collect { |item| BuildItem.for(item).update }
 end
 
 #----------------------------
