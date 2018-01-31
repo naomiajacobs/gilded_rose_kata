67311dff0a88d782ab677e635f0f7861f8fcbb94
Naomi Jacobs <naomi@mavenlink.com>
2018-01-31 09:40:12 -0800

    Use classes and subclasses
    Signed-off-by: Andrew Huth <ahuth@mavenlink.com>

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,48 +1,79 @@
-def update_quality(items)
-  items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
+class Updater
+  def initialize(item)
+    @item = item
+  end
+
+  def update_item
+    update_quality
+    update_sell_in
+  end
+
+  def update_quality
+    return if @item.quality == 0
+    decrease_by = @item.sell_in > 0 ? 1 : 2
+    @item.quality -= decrease_by
+  end
+
+  def update_sell_in
+    @item.sell_in -= 1
+  end
+end
+
+class SulfurasUpdater < Updater
+  def update_quality
+
+  end
+
+  def update_sell_in
+
+  end
+end
+
+class BrieUpdater < Updater
+  def update_quality
+    increase_by = @item.sell_in <= 0 ? 2 : 1
+    @item.quality = [50, @item.quality + increase_by].min
+  end
+end
+
+class BackStagePassUpdater < Updater
+  def update_quality
+    sell_in = @item.sell_in
+
+    return @item.quality = 0 if sell_in <= 0
+
+    if sell_in < 6
+      increase_by = 3
+    elsif sell_in < 11
+      increase_by = 2
     else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
+      increase_by = 1
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
-    end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
+
+    @item.quality = [50, @item.quality + increase_by].min
+  end
+end
+
+def update_quality(items)
+  types = {
+    sulfuras: "Sulfuras, Hand of Ragnaros",
+    brie: "Aged Brie",
+    backstage_pass: "Backstage passes to a TAFKAL80ETC concert",
+  }
+
+  items.each do |item|
+    updater_class = case item.name
+      when types[:sulfuras]
+        SulfurasUpdater
+      when types[:brie]
+        BrieUpdater
+      when types[:backstage_pass]
+        BackStagePassUpdater
       else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
+        Updater
     end
+
+    updater_class.new(item).update_item
   end
 end
 
